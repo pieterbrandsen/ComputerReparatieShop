b119f40d240bec71db47abfd4a5117292a6205e5
pieterbrandsen <45623682+pieterbrandsen@users.noreply.github.com>
2020-10-02 10:48:10 +0200

    ReperatieModel

--- ComputerReperatieShop/App_Start/RouteConfig.cs
+++ ComputerReperatieShop/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ComputerReperatieShop
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ReperatieOpdrachten", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ComputerReperatieShop/Models/ReperatieOpdracht.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ComputerReperatieShop.Models
+{
+    public class ReperatieOpdracht
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime StartDate { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime EndDate { get; set; }
+        public Status Status { get; set; }
+        public string Description { get; set; }
+        public bool Visible { get; set; }
+    }
+
+
+    public enum Status
+    {
+        Pending,
+        Underway,
+        [Display(Name = "Waiting for parts")]
+        WaitingForParts,
+        Done
+    }
+}

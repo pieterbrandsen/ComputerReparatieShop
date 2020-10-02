8070c0ce97e0348ef910b7dd24b42abe89a4b4dd
pieterbrandsen <45623682+pieterbrandsen@users.noreply.github.com>
2020-10-02 14:43:12 +0200

    All Repair Orders Table added

--- DAL/DAL/MockDB.cs
+++ DAL/DAL/MockDB.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ComputerRepairShop.Data.Models;
+using ComputerRepairShop.Classes.Helpers;
 
 namespace ComputerRepairShop.Data.DAL
 {
@@ -14,11 +15,11 @@ namespace ComputerRepairShop.Data.DAL
         {
             repairOrders = new List<RepairOrder>()
             {
-                new RepairOrder {Id = 0, Name = "Name0", Description = "Description0", Status = Status.Pending, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
-                new RepairOrder {Id = 1, Name = "Name1", Description = "Description1", Status = Status.Pending, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
-                new RepairOrder {Id = 2, Name = "Name2", Description = "Description2", Status = Status.Pending, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
-                new RepairOrder {Id = 3, Name = "Name3", Description = "Description3", Status = Status.Pending, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
-                new RepairOrder {Id = 4, Name = "Name4", Description = "Description4", Status = Status.Pending, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
+                new RepairOrder {Id = 0, Name = "Name0", Description = "Description0", Status = RepairOrderStatus.Done, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
+                new RepairOrder {Id = 1, Name = "Name1", Description = "Description1", Status = RepairOrderStatus.Pending, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
+                new RepairOrder {Id = 2, Name = "Name2", Description = "Description2", Status = RepairOrderStatus.WaitingForParts, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
+                new RepairOrder {Id = 3, Name = "Name3", Description = "Description3", Status = RepairOrderStatus.Underway, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
+                new RepairOrder {Id = 4, Name = "Name4", Description = "Description4", Status = RepairOrderStatus.Done, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
             };
         }
 

ecc5536c8304786d5d29dd7f2cec6b0cb78c6ab8
pieterbrandsen <45623682+pieterbrandsen@users.noreply.github.com>
2020-10-05 13:39:46 +0200

    New project

---
+++ ComputerReparatieShop/ComputerReparatieShop.Data/Migrations/202010050953451_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿namespace OdeToFoodData.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.RepairOrders",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        StartDate = c.DateTime(nullable: false),
+                        EndDate = c.DateTime(nullable: false),
+                        Status = c.Int(nullable: false),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.RepairOrders");
+        }
+    }
+}
---
+++ ComputerReparatieShop/ComputerReparatieShop.Data/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace OdeToFoodData.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ComputerRepairShop.Services.ComputerRepairShopDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ComputerRepairShop.Services.ComputerRepairShopDbContext";
+        }
+
+        protected override void Seed(ComputerRepairShop.Services.ComputerRepairShopDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}

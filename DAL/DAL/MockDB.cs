31f24138c95e2788dd89879e7e99842aa6004f07
P.F.W. de Bruin <pldbrn.117@gmail.com>
2020-10-05 17:21:30 +1200

    Styled "Index" of "RepairOrder" using bootstrap and a some inline css.

--- DAL/DAL/MockDB.cs
+++ DAL/DAL/MockDB.cs
@@ -15,11 +15,11 @@ namespace ComputerRepairShop.Data.DAL
         {
             repairOrders = new List<RepairOrder>()
             {
-                new RepairOrder {Id = 0, Name = "Name0", Description = "Description0", Status = RepairOrderStatus.Done, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
-                new RepairOrder {Id = 1, Name = "Name1", Description = "Description1", Status = RepairOrderStatus.Pending, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
-                new RepairOrder {Id = 2, Name = "Name2", Description = "Description2", Status = RepairOrderStatus.WaitingForParts, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
-                new RepairOrder {Id = 3, Name = "Name3", Description = "Description3", Status = RepairOrderStatus.Underway, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
-                new RepairOrder {Id = 4, Name = "Name4", Description = "Description4", Status = RepairOrderStatus.Done, StartDate = DateTime.Today, EndDate = DateTime.Today, Visible = false },
+                new RepairOrder {Id = 0, Name = "Name0", Description = "Description0", Status = RepairOrderStatus.Done, StartDate = DateTime.Today.Date, EndDate = DateTime.Today, Visible = false },
+                new RepairOrder {Id = 1, Name = "Name1", Description = "Description1", Status = RepairOrderStatus.Pending, StartDate = DateTime.Today.Date, EndDate = DateTime.Today, Visible = false },
+                new RepairOrder {Id = 2, Name = "Name2", Description = "Description2", Status = RepairOrderStatus.WaitingForParts, StartDate = DateTime.Today.Date, EndDate = DateTime.Today, Visible = false },
+                new RepairOrder {Id = 3, Name = "Name3", Description = "Description3", Status = RepairOrderStatus.Underway, StartDate = DateTime.Today.Date, EndDate = DateTime.Today, Visible = false },
+                new RepairOrder {Id = 4, Name = "Name4", Description = "Description4", Status = RepairOrderStatus.Done, StartDate = DateTime.Today.Date, EndDate = DateTime.Today, Visible = false },
             };
         }
 

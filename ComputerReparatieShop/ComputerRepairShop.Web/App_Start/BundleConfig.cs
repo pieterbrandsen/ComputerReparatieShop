aa3066aa03b698caac295a3d21dc61f00c5d076c
pieterbrandsen <45623682+pieterbrandsen@users.noreply.github.com>
2020-10-06 14:53:15 +0200

    OnStartupAdded

--- ComputerReparatieShop/ComputerRepairShop.Web/App_Start/BundleConfig.cs
+++ ComputerReparatieShop/ComputerRepairShop.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace ComputerRepairShop.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/custom.css"));
         }
     }
 }
